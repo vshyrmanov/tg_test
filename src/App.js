e47dd17b5708558a97ccb6a0cd6f0a5448fc5d16
vshyrmanov <shyrmanovvm@gmail.com>
2022-12-27 18:56:13 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import SimpleInput from './components/SimpleInput';
 import {useCallback, useEffect, useState} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+import { useRoutes } from 'react-router-dom';
+import Register from "./pages/Register";
+import Home from './pages/Home';
 
 function App() {
   const { telegramApi, onCloseHandler, user, onToggleButton  } = useTelegram()
@@ -34,15 +37,26 @@ function App() {
   }, [onSendData])
 
 
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: 'register', element: <Register /> },
+    // { path: 'usluge', element: <Services /> },
+    // { path: 'galerija', element: <Gallery /> },
+    // { path: 'cjenovnik', element: <Prices /> },
+    // { path: 'kontakt', element: <Contact /> }
+  ]);
+
 
   return (
-    <div className="app">
-      {/*<SimpleInput />*/}
-      <input value={test} onChange={(e) => setTest(e.target.value)} />
-      {user?.username && <span>{`Hello, ${user?.username}`}</span>}
-      {/*<button onClick={onToggleButton}>Довідка про склад родини</button>*/}
-      <button onClick={onCloseHandler}>Закрити</button>
-    </div>
+    routes
+    // <div className="app">
+    //
+    //   {/*<SimpleInput />*/}
+    //   {/*<input value={test} onChange={(e) => setTest(e.target.value)} />*/}
+    //   {/*{useEffectr?.username && <span>{`Hello, ${user?.username}`}</span>}*/}
+    //   {/*<button onClick={onToggleButton}>Довідка про склад родини</button>*/}
+    //   {/*<button onClick={onCloseHandler}>Закрити</button>*/}
+    // </div>
   );
 }
 
---
+++ src/pages/Register.jsx
@@ -0,0 +1,65 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {useTelegram} from "../hooks/useTelegram";
+
+
+const Register = () => {
+	const { telegramApi, onCloseHandler, user, onToggleButton  } = useTelegram();
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+
+	useEffect(() => {
+		telegramApi.ready();
+		// telegramApi.MainButton.show()
+	}, [])
+
+	useEffect(() => {
+		if (!firstName || !lastName) {
+			telegramApi.MainButton.hide()
+		} else {
+			telegramApi.MainButton.show()
+		}
+	}, [firstName, lastName])
+
+	const onSendData = useCallback(() => {
+		const data = {
+			firstName,
+			lastName,
+		}
+
+		telegramApi.sendData(JSON.stringify(data))
+	}, [firstName, lastName])
+
+	useEffect(() => {
+		telegramApi.MainButton.setParams({
+			text: 'Зареєструватись'
+		})
+	}, [])
+
+	useEffect(() => {
+		telegramApi.onEvent('mainButtonClicked', onSendData)
+		return () => {
+			telegramApi.offEvent('mainButtonClicked', onSendData)
+		}
+
+	}, [onSendData])
+
+	const handleFirstName = (e) => {
+		setFirstName(e.target.value)
+	}
+
+	const handleLastName = (e) => {
+		setLastName(e.target.value)
+	}
+
+
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column' }}>
+			<label>Вкажіть імя</label>
+			<input value={firstName} onChange={handleFirstName} />
+			<label>Вкажіть Прізвище</label>
+			<input value={lastName} onChange={handleLastName} />
+		</div>
+	);
+};
+
+export default Register;
