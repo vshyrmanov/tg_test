eb40407971c64b4e29e6e8ae06df34278cafd9f4
vshyrmanov <shyrmanovvm@gmail.com>
2022-12-27 09:30:29 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,46 +5,25 @@ import {useTelegram} from "./hooks/useTelegram";
 function App() {
   const { telegramApi, onCloseHandler, user, onToggleButton  } = useTelegram()
   const [test, setTest] = useState('');
-  const [show, setShow] = useState(false);
 
   useEffect(() => {
     telegramApi.ready();
+    telegramApi.MainButton.show()
   }, [])
 
-  useEffect(() => {
-    if (test.length > 3) {
-      onToggleButton()
+  const onSendData = useCallback(() => {
+    const data = {
+      title: test
     }
+
+    telegramApi.sendData(JSON.stringify(data))
   }, [test])
-
-  // const onSendData = useCallback(() => {
-    const data = {
-      title: test
-    }
-    telegramApi.sendData(JSON.stringify(data))
-  //   // onCloseHandler()
-  // }, [test])
-
-  const onSendData = () => {
-    const data = {
-      title: test
-    }
-    telegramApi.sendData(JSON.stringify(data))
-    setShow(prev => !prev)
-  }
 
   useEffect(() => {
     telegramApi.MainButton.setParams({
       text: 'Замовити документи'
     })
   }, [])
-
-  // useEffect(() => {
-  //   telegramApi.MainButton.onClick(onSendData)
-  //   return () => {
-  //     telegramApi.MainButton.offClick(onSendData)
-  //   }
-  // }, [])
 
   useEffect(() => {
     telegramApi.onEvent('mainButtonClicked', onSendData)
@@ -59,8 +38,7 @@ function App() {
       {/*<SimpleInput />*/}
       <input value={test} onChange={(e) => setTest(e.target.value)} />
       {user?.username && <span>{`Hello, ${user?.username}`}</span>}
-      {show && <span>GOOD JOB!!!</span>}
-
+      <button>Send data</button>
       {/*<button onClick={onToggleButton}>Довідка про склад родини</button>*/}
       <button onClick={onCloseHandler}>Закрити</button>
     </div>
