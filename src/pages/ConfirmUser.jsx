d94574de1944da7868d24a6dd6a086c6358a4098
vshyrmanov <shyrmanovvm@gmail.com>
2023-01-04 23:40:39 +0200

    test

--- src/pages/ConfirmUser.jsx
+++ src/pages/ConfirmUser.jsx
@@ -6,17 +6,18 @@ const ConfirmUser = () => {
 	const data = search?.search.split('&').map(e => ({[e.split('=')[0].slice(1)]: e.split('=')[1]}));
 	return (
 		<div>
-			<h1>Confirm user</h1>
-			{data?.map(user =>
-				<React.Fragment>
-					<span>{user.chatId}</span>
-					<span>{user.firstName}</span>
-					<span>{user.secondName}</span>
-					<span>{user.thirdName}</span>
-					<span>{user.personalNumber}</span>
-					<span>{user.rank}</span>
-				</React.Fragment>
-			)}
+			<h1>Confirm user2</h1>
+			<span>{search.search}</span>
+			{/*{data?.map(user =>*/}
+			{/*	<React.Fragment>*/}
+			{/*		<span>{user.chatId}</span>*/}
+			{/*		<span>{user.firstName}</span>*/}
+			{/*		<span>{user.secondName}</span>*/}
+			{/*		<span>{user.thirdName}</span>*/}
+			{/*		<span>{user.personalNumber}</span>*/}
+			{/*		<span>{user.rank}</span>*/}
+			{/*	</React.Fragment>*/}
+			{/*)}*/}
 		</div>
 	);
 };
